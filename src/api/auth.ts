2dfc04ec8fe79dd80bd4b6a4482133f8b44334b6
Hasan A. Tekeoğlu <55619655+tekeoglan@users.noreply.github.com>
2023-01-31 10:38:59 +0300

    chor(): add auth query hooks

---
+++ src/api/auth.ts
@@ -0,0 +1,10 @@
+import { useCreate } from "../hooks/reactQuery";
+import { apiRoutes } from "../constants/routes";
+
+export const useLogin = () => useCreate<any, any>(apiRoutes.login);
+
+export const useRegister = () => useCreate<any, any>(apiRoutes.register);
+
+export const useLogOut = () => useCreate<any, any>(apiRoutes.logOut);
+
+export const useFetchUser = () => useCreate<any, any>(apiRoutes.fetchUser);
