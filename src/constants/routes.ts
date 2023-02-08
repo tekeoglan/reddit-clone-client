6b42ce0797e6215d7da79195a1d51d8d735066fa
Hasan A. Tekeoğlu <55619655+tekeoglan@users.noreply.github.com>
2023-02-08 14:16:30 +0300

    chor(): switch aws to railway for hosting

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,7 +1,7 @@
 const baseUrl =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : "http://3.125.43.131:3000";
+    : "reddit-clone-server-production.up.railway.app";
 
 export const apiRoutes = {
   login: `${baseUrl}/login`,
