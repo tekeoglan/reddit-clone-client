2c0e475d265dd1c34f21c2feb5296668c077b327
Hasan A. Tekeoğlu <55619655+tekeoglan@users.noreply.github.com>
2024-01-12 16:15:19 +0300

    fix(): try to resolve serve url issiue

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,7 +1,7 @@
 const baseUrl =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : process.env.SERVER_URL;
+    : import.meta.VITE_SERVER_URL;
 
 export const apiRoutes = {
   login: `${baseUrl}/login`,
